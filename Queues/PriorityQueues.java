665a5eddddc6271c0ce04094e0cd90eeea827768
alecmarkmorris <Alecmarkmorris@gmail.com>
2023-09-26 23:13:08 -0700

    Adding Priority Queues file to Queues folder

---
+++ Queues/PriorityQueues.java
@@ -0,0 +1,33 @@
+package Queues;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+public class PriorityQueues {
+
+    public static void main(String[] args){
+        
+        //Priority Queue = FIFO data structure, First-In First-Out
+        //                  stored objects into a sort of "Horizontal Tower"
+        //                  First element added to the queue will be the first element removed from the queue
+        //                  The elements are sorted in the order that they are added
+        
+        //Initializing a new Priority Queue
+        Queue<String> queue = new PriorityQueue<>(Collections.reverseOrder());
+        
+        //Adding elements to the queue
+        queue.offer("F");
+        queue.offer("B");
+        queue.offer("C");
+        queue.offer("A");
+        queue.offer("D");
+
+        //While queue is not empty poll the DS
+        while(!queue.isEmpty()){
+            System.out.println(queue.poll());
+        }
+
+        
+    }
+}
